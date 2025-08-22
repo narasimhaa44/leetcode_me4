b78131f55df1bcddf6b93ceb331c8c5af14f675f
Challa Lakshmi  Narasimha Reddy <lakshminarasimh44@gmail.com>
2025-08-22 21:48:25 +0530

    Time: 1 ms (59.7%), Space: 44.6 MB (96.69%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int l = 1;  // pointer for unique position
+        for (int r = 1; r < nums.length; r++) {
+            if (nums[r] != nums[r - 1]) {
+                nums[l] = nums[r];
+                l++;
+            }
+        }
+        return l;
+    }
+}
