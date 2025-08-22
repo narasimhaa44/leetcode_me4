69b68ac31dad41e6b73441ddab853b83071cb3dc
Challa Lakshmi  Narasimha Reddy <lakshminarasimh44@gmail.com>
2025-08-22 22:10:45 +0530

    Time: 431 ms (5%), Space: 45.9 MB (85.96%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,7 +2,7 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int temp;
         for(int i=0;i<nums.length-1;i++){
-            for(int j=0;j<nums.lenght-1;j++){
+            for(int j=0;j<nums.length-1;j++){
                 if(nums[j]==0){
                     temp=nums[j];
                     nums[j]=nums[j+1];
