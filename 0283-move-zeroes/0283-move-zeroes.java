7e2eae2b333ca514cb66ee56d40d6c1254b661ce
Challa Lakshmi  Narasimha Reddy <lakshminarasimh44@gmail.com>
2025-08-22 22:07:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int temp;
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=0;j<nums.lenght-1;j++){
+                if(nums[j]==0){
+                    temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                }
+            }
+        }
+    }
+}
