edc7919c7265566da2744c969b86931adfc0d824
Challa Lakshmi  Narasimha Reddy <lakshminarasimh44@gmail.com>
2025-11-20 12:52:18 +0530

    Time: 8 ms (31.13%), Space: 47 MB (22.92%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -0,0 +1,56 @@
+// class Solution {
+//     public int ways(int i,int j,int[][] matrix,int m,int n,int[][] dp){
+//         if(i>=m || j>=n || i<0 || j<0){
+//             return Integer.MAX_VALUE;
+//         }
+//         if(i==m-1){
+//             return matrix[i][j];
+//         }
+//         if(dp[i][j]!=0){
+//             return dp[i][j];
+//         }
+//         int down=ways(i+1,j,matrix,m,n,dp);
+//         int diagr=ways(i+1,j+1,matrix,m,n,dp);
+//         int diagl=ways(i+1,j-1,matrix,m,n,dp);
+//         dp[i][j]=matrix[i][j]+Math.min(down,Math.min(diagr,diagl));
+//         return dp[i][j];
+//     }
+//     public int minFallingPathSum(int[][] matrix) {
+//         int m=matrix.length;
+//         int n=matrix[0].length;
+//         int sum=0;
+//         int min=Integer.MAX_VALUE;
+//         int[][] dp=new int[m][n];
+//         for(int i=0;i<n;i++){
+//             sum=ways(0,i,matrix,m,n,dp);
+//             min=Math.min(min,sum);
+//         }
+//         return min;
+//     }
+// }
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        for (int j = 0; j < n; j++) {
+            dp[m - 1][j] = matrix[m - 1][j];
+        }
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+
+                int down = dp[i + 1][j];
+
+                int diagLeft = (j - 1 >= 0) ? dp[i + 1][j - 1] : Integer.MAX_VALUE;
+                int diagRight = (j + 1 < n) ? dp[i + 1][j + 1] : Integer.MAX_VALUE;
+
+                dp[i][j] = matrix[i][j] + Math.min(down, Math.min(diagLeft, diagRight));
+            }
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int j = 0; j < n; j++) {
+            ans = Math.min(ans, dp[0][j]);
+        }
+        return ans;
+    }
+}
