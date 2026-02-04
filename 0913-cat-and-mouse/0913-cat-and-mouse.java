0b214503e0a17442cff5f5b20aec5aa4795d1416
Challa Lakshmi  Narasimha Reddy <lakshminarasimh44@gmail.com>
2026-02-04 23:47:05 +0530

    Time: 45 ms (63.13%), Space: 47.6 MB (45.62%) - LeetHub

--- 0913-cat-and-mouse/0913-cat-and-mouse.java
+++ 0913-cat-and-mouse/0913-cat-and-mouse.java
@@ -1,57 +1,77 @@
 class Solution {
-
     static final int DRAW = 0;
     static final int MOUSE = 1;
     static final int CAT = 2;
 
-    int[][] graph;
-    int[][][] dp;
-    int n;
-    int MAX_MOVES;
-
     public int catMouseGame(int[][] graph) {
-        this.graph = graph;
-        this.n = graph.length;
-        this.dp = new int[n][n][2];
-        this.MAX_MOVES = 2 * n * n;
+        int n = graph.length;
 
-        return dfs(1, 2, 0, 0);
-    }
+        int[][][] color = new int[n][n][2];
+        int[][][] degree = new int[n][n][2];
 
-    int dfs(int mouse, int cat, int turn, int moves) {
+        // initialize degrees
+        for (int m = 0; m < n; m++) {
+            for (int c = 0; c < n; c++) {
+                degree[m][c][0] = graph[m].length;
+                degree[m][c][1] = graph[c].length;
+                for (int x : graph[c]) {
+                    if (x == 0) degree[m][c][1]--;
+                }
+            }
+        }
 
-        // move limit → draw
-        if (moves >= MAX_MOVES) return DRAW;
+        Queue<int[]> q = new LinkedList<>();
 
         // base cases
-        if (mouse == 0) return MOUSE;
-        if (mouse == cat) return CAT;
-
-        if (dp[mouse][cat][turn] != 0)
-            return dp[mouse][cat][turn];
-
-        if (turn == 0) { // Mouse turn
-            boolean canDraw = false;
-            for (int next : graph[mouse]) {
-                int res = dfs(next, cat, 1, moves + 1);
-                if (res == MOUSE)
-                    return dp[mouse][cat][turn] = MOUSE;
-                if (res == DRAW)
-                    canDraw = true;
+        for (int i = 0; i < n; i++) {
+            for (int t = 0; t < 2; t++) {
+                if (i != 0) {
+                    color[0][i][t] = MOUSE;
+                    q.offer(new int[]{0, i, t});
+                }
+                color[i][i][t] = CAT;
+                q.offer(new int[]{i, i, t});
             }
-            return dp[mouse][cat][turn] = canDraw ? DRAW : CAT;
-        } 
-        else { // Cat turn
-            boolean canDraw = false;
-            for (int next : graph[cat]) {
-                if (next == 0) continue;
-                int res = dfs(mouse, next, 0, moves + 1);
-                if (res == CAT)
-                    return dp[mouse][cat][turn] = CAT;
-                if (res == DRAW)
-                    canDraw = true;
-            }
-            return dp[mouse][cat][turn] = canDraw ? DRAW : MOUSE;
         }
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int m = cur[0], c = cur[1], t = cur[2];
+            int curColor = color[m][c][t];
+
+            for (int[] prev : parents(graph, m, c, t)) {
+                int pm = prev[0], pc = prev[1], pt = prev[2];
+                if (color[pm][pc][pt] != DRAW) continue;
+
+                if ((pt == 0 && curColor == MOUSE) ||
+                    (pt == 1 && curColor == CAT)) {
+                    color[pm][pc][pt] = curColor;
+                    q.offer(new int[]{pm, pc, pt});
+                } else {
+                    degree[pm][pc][pt]--;
+                    if (degree[pm][pc][pt] == 0) {
+                        color[pm][pc][pt] = (pt == 0) ? CAT : MOUSE;
+                        q.offer(new int[]{pm, pc, pt});
+                    }
+                }
+            }
+        }
+
+        return color[1][2][0];
+    }
+
+    private List<int[]> parents(int[][] graph, int m, int c, int t) {
+        List<int[]> res = new ArrayList<>();
+        if (t == 1) {
+            for (int pm : graph[m]) {
+                res.add(new int[]{pm, c, 0});
+            }
+        } else {
+            for (int pc : graph[c]) {
+                if (pc != 0)
+                    res.add(new int[]{m, pc, 1});
+            }
+        }
+        return res;
     }
 }
