8645bee7f83738b9969760deb9fbb7f23ec45738
Challa Lakshmi  Narasimha Reddy <lakshminarasimh44@gmail.com>
2026-02-13 14:49:15 +0530

    Time: 20 ms (79.69%), Space: 47.1 MB (68.66%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int ways(int i, int j, String s, int[][] dp) {
+        if (i > j) return 0;
+        if (i == j) return 1;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (s.charAt(i) == s.charAt(j)) {
+            dp[i][j] = 2 + ways(i + 1, j - 1, s, dp);
+        } else {
+            dp[i][j] = Math.max(
+                ways(i + 1, j, s, dp),
+                ways(i, j - 1, s, dp)
+            );
+        }
+
+        return dp[i][j];
+    }
+
+    public int minInsertions(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for (int[] row : dp) {
+            java.util.Arrays.fill(row, -1);
+        }
+
+        int lps = ways(0, n - 1, s, dp);
+        return n - lps;
+    }
+}
