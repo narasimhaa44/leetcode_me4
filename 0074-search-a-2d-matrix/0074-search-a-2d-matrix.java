131e1beb87154eecc93c58616f31d6ad650d4a6b
Challa Lakshmi  Narasimha Reddy <lakshminarasimh44@gmail.com>
2026-03-29 16:22:26 +0530

    Time: 0 ms (100%), Space: 44.2 MB (16.72%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -2,14 +2,14 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m=matrix.length;
         int n=matrix[0].length;
-        int row=0;
+        int row=-1;
         for(int i=0;i<m;i++){
             if(matrix[i][n-1]>=target){
                 row=i;
                 break;
             }
         }
-        
+        if(row==-1) return false;
         for(int j=0;j<n;j++){
             if(matrix[row][j]==target){
                 return true;
