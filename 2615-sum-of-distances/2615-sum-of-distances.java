b0d8db902aab95d621e39956091a44a93a952562
Challa Lakshmi  Narasimha Reddy <lakshminarasimh44@gmail.com>
2026-04-23 10:50:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2615-sum-of-distances/2615-sum-of-distances.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long[] distance(int[] nums) {
+        long[] res=new long[nums.length];
+       for(int i=0;i<nums.length;i++){
+        int sum=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[i]==nums[j] && i!=j){
+                sum+=Math.abs(i-j);
+            }  
+         }
+         res[i]=sum;
+       }
+       return res; 
+    }
+}
