308fb5f9285a850a88203d5002a76587436f8346
Challa Lakshmi  Narasimha Reddy <lakshminarasimh44@gmail.com>
2026-06-21 22:45:20 +0530

    Time: 4 ms (99.23%), Space: 81.9 MB (97.37%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -37,17 +37,26 @@
 // }
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);  // sort prices
+        int max = 100000;
+        int[] freq = new int[max + 1];
+        
+        // count frequency
+        for (int cost : costs) {
+            freq[cost]++;
+        }
         
         int count = 0;
         
-        for (int cost : costs) {
-            if (coins >= cost) {
-                coins -= cost;
-                count++;
-            } else {
-                break;
-            }
+        // try buying from cheapest
+        for (int cost = 1; cost <= max; cost++) {
+            if (freq[cost] == 0) continue;
+            
+            int canBuy = Math.min(freq[cost], coins / cost);
+            
+            count += canBuy;
+            coins -= canBuy * cost;
+            
+            if (coins < cost) break;
         }
         
         return count;
