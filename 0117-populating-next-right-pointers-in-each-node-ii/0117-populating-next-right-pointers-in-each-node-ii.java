d0dbc873818c19376958cb8e71229c8f366ea3f9
Challa Lakshmi  Narasimha Reddy <lakshminarasimh44@gmail.com>
2026-07-03 16:49:51 +0530

    Time: 0 ms (100%), Space: 45.8 MB (97.41%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -27,29 +27,64 @@ class Node {
 //     }
 // }
 
+// class Solution {
+//     public Node connect(Node root) {
+//         if(root==null) return null;
+//         Queue<Node> q=new LinkedList<>();
+//         q.add(root);
+//         while(!q.isEmpty()){
+//             int l=q.size();
+//             for(int i=0;i<l;i++){
+//                 Node p=q.poll();
+//                 if(i!=l-1){
+//                         p.next=q.peek();
+//                 }
+//                 else {
+//                     p.next=null;
+//                 }
+//                 if(p.left!=null){
+//                         q.add(p.left);
+//                 }
+//                 if(p.right!=null){
+//                     q.add(p.right);
+//                 }
+//             }
+//         }
+//         return root;
+//     }
+// }
 class Solution {
+
     public Node connect(Node root) {
-        if(root==null) return null;
-        Queue<Node> q=new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int l=q.size();
-            for(int i=0;i<l;i++){
-                Node p=q.poll();
-                if(i!=l-1){
-                        p.next=q.peek();
+
+        if(root == null)
+            return null;
+
+        Node curr = root;
+
+        while(curr != null){
+
+            Node dummy = new Node(0);
+            Node tail = dummy;
+
+            while(curr != null){
+
+                if(curr.left != null){
+                    tail.next = curr.left;
+                    tail = tail.next;
                 }
-                else {
-                    p.next=null;
+
+                if(curr.right != null){
+                    tail.next = curr.right;
+                    tail = tail.next;
                 }
-                if(p.left!=null){
-                        q.add(p.left);
-                }
-                if(p.right!=null){
-                    q.add(p.right);
-                }
+
+                curr = curr.next;
             }
+
+            curr = dummy.next;
         }
+
         return root;
     }
 }
